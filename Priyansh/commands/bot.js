9580574f243523546862cd5382b217a2b7ffc9b1
k3500047-source <k3500047@gmail.com>
2026-01-06 16:46:32 +0530

    Update bot.js

--- Priyansh/commands/bot.js
+++ Priyansh/commands/bot.js
@@ -22,7 +22,8 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
    mess = "{name}"
   if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
     var msg = {
-      body: `${name},『\n${rand}`
+      body: `${name}, \n ${rand}`
+      //`🔶${name}🔶,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : 𝗔𝗿𝘂𝗻 𝗞𝘂𝗺𝗮𝗿🌹 `
     }
     return api.sendMessage(msg, threadID, messageID);
   };
